efd2fd3f3a1e1183cf72a190b702773c8637c716
Mark <twcloudwebapp@outlook.com>
2016-08-02 13:30:50 +0800

    add filter and search

--- kunshan/polls/admin.py
+++ kunshan/polls/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # admin.site.register(Question)
-class ChoiceInline(admin.StackedInline):
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+
     model = Choice
     extra = 3
 class QuestionAdmin(admin.ModelAdmin):
@@ -15,4 +17,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+
 admin.site.register(Question, QuestionAdmin)
