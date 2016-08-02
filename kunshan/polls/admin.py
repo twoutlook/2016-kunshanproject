f4ae6efc0cc15b4c988e00dcc4e28de31c21b040
Mark <twcloudwebapp@outlook.com>
2016-08-02 13:20:07 +0800

    show subform to complete question and choice

--- kunshan/polls/admin.py
+++ kunshan/polls/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # admin.site.register(Question)
-
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
-    fields = [ 'question_text','pub_date']
+    # fields = [ 'question_text','pub_date']
 
+    fieldsets = [
+        ('問題︰',               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
 admin.site.register(Question, QuestionAdmin)
