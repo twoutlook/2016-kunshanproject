39b228b87568089542c4e11f6eb12944c9d1727e
Mark <twcloudwebapp@outlook.com>
2016-08-02 11:06:31 +0800

    start test, python manage.py test polls

--- kunshan/polls/models.py
+++ kunshan/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
+import datetime
 @python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
